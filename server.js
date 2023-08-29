888cac616e71fc7c1f16a84e39c32bc55141a0eb
Jordon <jordonmeril9@gmail.com>
2023-08-29 15:39:11 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-// Load express
-const express = require('express');
+const express = require('express')
+require('dotenv').config()
+const path = require('path')
+const middleware = require('./utils/middleware')
 
-// Create our express app
-const app = express();
+// we'll build a middleware function to user in this file
 
-// Define a "root" route directly on app
-// Tomorrow, we'll use best practice routing
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+// our routers/controllers will be imported here
+const AuthRouter = require('./controllers/authControllers')
+const DriverRouter = require('./controllers/driverControllers')
+const CommentRouter = require('./controllers/commentControllers')
+
+const app = express()
+
+// set up our view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/////////////////////
+//// Middleware  ////
+/////////////////////
+middleware(app)
+
+////////////////
+//// Routes ////
+////////////////
+app.get('/', (req, res) => {
+    res.redirect('/drivers')
+
+})
+app.get('/drivers', function (req, res) {
+    res.send('<h1>Hello Drivers!</h1>');
 });
-
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.get('/comments', function (req, res) {
+    res.send('<h1>Hello Comments!</h1>');
 });
-// Test
+  
+  
+
+app.use('/', AuthRouter)
+app.use('/drivers', DriverRouter)
+app.use('/comments', CommentRouter)
+
+//////////////////////////
+//// Server Listener  ////
+//////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log('DriveHive is ready to go')
+})
